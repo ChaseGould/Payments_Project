9dc0e46d522fff6f5f42a6ede82ec46e12330f14
ChaseGould <chase.gould7@gmail.com>
2019-11-25 01:37:20 -0800

    Left off right before 1:07:43 where the constructor argument causes webpage to not load form.

--- testingAngular/src/app/shared/payment-detail.service.ts
+++ testingAngular/src/app/shared/payment-detail.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { PaymentDetail } from './payment-detail.model';
+import { HttpClient, HttpRequest } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +10,8 @@ export class PaymentDetailService {
   formData:PaymentDetail
 
   constructor() { }
+
+  postPaymentDetail(formData:PaymentDetail){
+
+  }
 }
