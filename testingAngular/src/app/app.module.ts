1e1d1ef36e243786dcddf4dadb293d930aa2bd99
ChaseGould <chase.gould7@gmail.com>
2019-12-13 20:15:04 -0800

    added login, registration components and auth service

--- testingAngular/src/app/app.module.ts
+++ testingAngular/src/app/app.module.ts
@@ -10,18 +10,40 @@ import { PaymentDetailListComponent } from './payment-details/payment-detail-lis
 import { PaymentDetailService } from './shared/payment-detail.service';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { LoginComponent } from './auth/login/login.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAilWKb9vw5VWTGZNV0WNTmiLtYbo5uhug",
+  authDomain: "fir-auth-52205.firebaseapp.com",
+  databaseURL: "https://fir-auth-52205.firebaseio.com",
+  projectId: "fir-auth-52205",
+  storageBucket: "fir-auth-52205.appspot.com",
+  messagingSenderId: "641295054798",
+  appId: "1:641295054798:web:e9a7ae512d1bc579bfdd9f",
+  measurementId: "G-9F8SE6KTRZ"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
     PaymentDetailsComponent,
     PaymentDetailComponent,
-    PaymentDetailListComponent
+    PaymentDetailListComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp( firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [PaymentDetailService],
   bootstrap: [AppComponent]
